f9ac2f9893bb029283a8525fed4b576fcaa3c52a
s4ntifdz <santiago.fernandez.utn@gmail.com>
2023-12-06 19:42:06 -0300

    Carrousel works, but the motherfucking JS doesnt want to do it

--- airlines/airlines/urls.py
+++ airlines/airlines/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('web.urls')),
+    path('', include('web.urls')),
 ]
 if settings.DEBUG: #aca pregunto si estoy en debug, ya que nunca me dejaria hacerlo en produccion
     from django.conf.urls.static import static
