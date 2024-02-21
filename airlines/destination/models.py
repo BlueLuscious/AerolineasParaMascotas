aaa1971d2eebfb0074427e14da4bed0da7986797
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-02-21 03:11:05 -0300

    update model & admin.
    refined for the final consumer.

--- airlines/destination/models.py
+++ airlines/destination/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class DestinationModel(models.Model):
-    country = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    airport = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/', default='images/default_product.png', blank=True, null=True)
+    country = models.CharField(max_length=100, verbose_name="País")
+    city = models.CharField(max_length=100, verbose_name="Ciudad")
+    airport = models.CharField(max_length=100, verbose_name="Aeropuerto")
+    image = models.ImageField(upload_to='images/', default='images/default_product.png', blank=True, null=True, verbose_name="Imagen")
     
     def __str__(self): 
-        return f"{self.country} - {self.city} - {self.airport} - {self.image}"
+        return f"{self.country} - {self.city}"
