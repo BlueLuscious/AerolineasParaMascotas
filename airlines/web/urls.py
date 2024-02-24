1cbb728d9c47487493169c9e5c1c2360e4473138
s4ntifdz <santiago.fernandez.utn@gmail.com>
2024-02-24 06:25:34 -0300

    Base html creado, Razas html creado, categoryview creado

--- airlines/web/urls.py
+++ airlines/web/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from web.views.index_view import IndexView
-# from airlines.web.views.category_view import CategoryView
+from web.views.category_view import CategoryView
 
 urlpatterns = [
     path('index/', IndexView.as_view(), name="index"),
-    # path('Raza/', CategoryView.as_view(), name="index"),
+    path('Category/<int:boton_id>/', CategoryView.as_view(), name="Category"),
 ]
--- airlines/web/views/category_view.py
+++ airlines/web/views/category_view.py
@@ -1,16 +1,9 @@
-from django.http import HttpResponse
-# from django.shortcuts import redirect
+from django.shortcuts import render
 from django.views import View
-from django.template import loader
-
-from destination.services import DestinationServices
-
-template = loader.get_template('Razas.html')
 
 class CategoryView(View):
-    def get(self, request):
-        context=[]
-        return HttpResponse(template.render(context, request))
-    
-    def post(self, request):
-        return HttpResponse(template.render(None, request))
+    def get(self, request, boton_id):
+        if boton_id == 1:
+            return render(request, 'Razas.html')
+        elif boton_id == 2:
+            return render(request, 'Paises.html')
