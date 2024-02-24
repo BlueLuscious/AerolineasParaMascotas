9696cd69984bc45d974879f8ac7580a70d198b1d
s4ntifdz <santiago.fernandez.utn@gmail.com>
2024-02-24 05:53:27 -0300

    no me acuerdo nada

--- airlines/web/urls.py
+++ airlines/web/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from web.views.index_view import IndexView
+# from airlines.web.views.category_view import CategoryView
 
 urlpatterns = [
     path('index/', IndexView.as_view(), name="index"),
+    # path('Raza/', CategoryView.as_view(), name="index"),
 ]
