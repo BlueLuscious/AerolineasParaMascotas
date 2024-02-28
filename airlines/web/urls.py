1367f0a25f40afe628b47c68da8dc14de8fabc7d
s4ntifdz <santiago.fernandez.utn@gmail.com>
2024-02-28 06:41:29 -0300

    Cambio de color en la barra, agregue paises y una vista

--- airlines/web/urls.py
+++ airlines/web/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from web.views.index_view import IndexView
 from web.views.category_view import CategoryView
+from web.views.subcategory_view import SubcategoryView
 
 urlpatterns = [
     path('index/', IndexView.as_view(), name="index"),
     path('Category/<int:boton_id>/', CategoryView.as_view(), name="Category"),
+    path('Subcategory/<int:boton_id>/', SubcategoryView.as_view(), name="Subcategory"),
 ]
---
+++ airlines/web/views/subcategory_view.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views import View
+
+class SubcategoryView(View):
+    def get(self, request, boton_id):
+        if boton_id == 1:
+            return render(request, 'Requisitos.html')
+        elif boton_id == 2:
+            return render(request, 'Continents/Europa.html')
+        elif boton_id == 3:
+            return render(request, 'Viaja_tranquilo.html')
+        elif boton_id == 4:
+            return render(request, 'Requisitos.html')
