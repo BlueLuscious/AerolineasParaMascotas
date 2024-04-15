dc37dae15e215a4d3503f12d41c82a3fc13a4306
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-04-15 00:22:41 -0300

    Update --> Index View
    Comment context.

--- airlines/web/views/index_view.py
+++ airlines/web/views/index_view.py
@@ -9,8 +9,8 @@ template = loader.get_template('index.html')
 
 class IndexView(View):
     def get(self, request):
-        context = DestinationServices.get_context()
-        return HttpResponse(template.render(context, request))
+        # context = DestinationServices.get_context()
+        return HttpResponse(template.render(None, request))
     
     def post(self, request):
         return HttpResponse(template.render(None, request))
