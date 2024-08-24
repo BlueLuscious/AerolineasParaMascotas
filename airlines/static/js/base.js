ef61854bd4c5e6caac7335ddae382ea2772866eb
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-08-24 16:15:20 -0300

    Sub menu para Sevicios
    Se creo un submenu que aparece al hacer click en Servicios, por el momento.
    
    Si deepues mas items del navbar tendran sub menus se hara un componente y se refatoraizara el javascript.

--- airlines/static/js/base.js
+++ airlines/static/js/base.js
@@ -7,17 +7,28 @@ document.addEventListener("DOMContentLoaded", () => {
     const sideNavbarMenu = document.getElementById("side_navbar_menu")
     const closeMenu = document.getElementById("btn_close_menu")
 
-    toggleNavbarByClick(openMenu, sideNavbarMenu)
-    toggleNavbarByClick(closeMenu, sideNavbarMenu)
+    toggleElementByClick(openMenu, sideNavbarMenu, "translate-x-full")
+    toggleElementByClick(closeMenu, sideNavbarMenu, "translate-x-full")
 
     
     const whatsappButton = document.getElementById("whatsapp_button")
     wa_service.redirectToWA(whatsappButton, 0)
+    
+    
+    /* SubMenu Servicios */
+    const navItemServices = document.getElementById("nav_item_services")
+    const navItemServicesContent = navItemServices.querySelector(".item_content")
+    const sub_menu = navItemServices.querySelector(".sub_menu")
+    const closeSubMenu = document.getElementById("btn_close_sub_menu")
+
+    toggleElementByClick(navItemServicesContent, sub_menu, "hidden")
+    toggleElementByClick(closeSubMenu, sub_menu, "hidden")
+
 
 })
 
-function toggleNavbarByClick(button, navbar) {
+function toggleElementByClick(button, element, tailwind_class) {
     button.addEventListener("click", () => {
-        navbar.classList.toggle("translate-x-full")
+        element.classList.toggle(tailwind_class)
     })
 }
