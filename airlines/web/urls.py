68bb71c46e2a79ce12ca955b468d92763a72347d
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-09-12 20:10:23 -0300

    Create --> Travel Quietly View
    Create --> View & Template.
    Declare --> URL for View.
    
    Add --> Redirects in Navbars.

--- airlines/web/urls.py
+++ airlines/web/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from web.views.index_view import IndexView
 from web.views.race_distintion_view import RaceDistintionView
+from web.views.travel_quietly_view import TravelQuietlyView
 from web.views.travel_restriction_view import TravelRestrictionView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("sin-distincion-de-raza/", RaceDistintionView.as_view(), name="race-distintion"),
+    path("viaja-tranquilo/", TravelQuietlyView.as_view(), name="travel-quietly"),
     path("restricciones-por-raza/", TravelRestrictionView.as_view(), name="travel-restriction"),
 ]
