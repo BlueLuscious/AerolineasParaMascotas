ded217bc25e09c13ebd43a38ad38e3d7a8db744a
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-09-13 00:32:52 -0300

    Fix Word

--- airlines/web/urls.py
+++ airlines/web/urls.py
@@ -7,6 +7,6 @@ from web.views.travel_restriction_view import TravelRestrictionView
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("sin-distincion-de-raza/", RaceDistintionView.as_view(), name="race-distintion"),
-    path("viaja-tranquilo/", TravelQuietlyView.as_view(), name="travel-quietly"),
+    path("viaje-tranquilo/", TravelQuietlyView.as_view(), name="travel-quietly"),
     path("restricciones-por-raza/", TravelRestrictionView.as_view(), name="travel-restriction"),
 ]
