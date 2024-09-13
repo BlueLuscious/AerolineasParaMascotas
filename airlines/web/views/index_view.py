c25fdad9c2c21bdeba3bab5f8589e454ce350c65
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-09-13 02:31:44 -0300

    Enhance Code --> Views

--- airlines/web/views/index_view.py
+++ airlines/web/views/index_view.py
@@ -1,13 +1,12 @@
 from django.http import HttpResponse
-# from django.shortcuts import redirect
 from django.views import View
 from django.template import loader
 
-from destination.services import DestinationServices
-
 template = loader.get_template("pages/index.html")
 
+
 class IndexView(View):
+    
     def get(self, request):
         return HttpResponse(template.render(None, request))
     
--- airlines/web/views/travel_quietly_view.py
+++ airlines/web/views/travel_quietly_view.py
@@ -2,7 +2,9 @@ from django.http import HttpRequest, HttpResponse
 from django.template import loader
 from django.views import View
 
+
 class TravelQuietlyView(View):
+    
     def get(self, request: HttpRequest) -> HttpResponse:
         template = loader.get_template("pages/before-fly/travel-quietly.html")
         return HttpResponse(template.render(None, request))
