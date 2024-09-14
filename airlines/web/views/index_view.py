b334a89492f76b1b019b6967de0a66c157bfbdd3
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-09-14 04:55:44 -0300

    Update --> index.html
    Se actualizo el index.html.
    
    Tambien se actualizaron otros templates.
    
    Ademas, se modificaron y borraron imagenes.

--- airlines/web/views/index_view.py
+++ airlines/web/views/index_view.py
@@ -2,14 +2,10 @@ from django.http import HttpResponse
 from django.views import View
 from django.template import loader
 
-template = loader.get_template("pages/index.html")
-
 
 class IndexView(View):
     
     def get(self, request):
-        return HttpResponse(template.render(None, request))
-    
-    def post(self, request):
+        template = loader.get_template("pages/index.html")
         return HttpResponse(template.render(None, request))
     
