8964042a2b3e9b368de3726db6ab17816d1e53ff
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-10-23 16:03:39 -0300

    Create --> Review Template
    - Include --> review.html in index.html.
    - Return --> ReviewModel in context (IndexView).

--- airlines/web/views/index_view.py
+++ airlines/web/views/index_view.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
 from django.views import View
 from django.template import loader
+from review.models import ReviewModel
 
 
 class IndexView(View):
     
     def get(self, request):
         template = loader.get_template("pages/index.html")
-        return HttpResponse(template.render(None, request))
+        context = {
+            "reviews": ReviewModel.objects.all().order_by("-created_at")[:6]
+        }
+        return HttpResponse(template.render(context, request))
     
