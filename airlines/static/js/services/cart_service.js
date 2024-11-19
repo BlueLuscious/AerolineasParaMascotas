740ba147bb6e6e190ec579369744c7218e0376be
S4ntifdz <safernandez@frro.utn.edu.ar>
2024-11-19 20:37:08 -0300

    refact: se pasa la instancia como argumento

--- airlines/static/js/services/cart_service.js
+++ airlines/static/js/services/cart_service.js
@@ -1,6 +1,6 @@
 export class CartService{
 
-    static addProduct(id,name,price){
-        Unicorn.call('cart','add_product', id, name, price)
+    static addProduct(id){
+        Unicorn.call('cart','add_product', id)
     }
 }
--- airlines/store/components/cart.py
+++ airlines/store/components/cart.py
@@ -1,20 +1,22 @@
 from django_unicorn.components import UnicornView
 
+from store.models import ProductModel
+
 
 class CartView(UnicornView):
     
     selected_products = []
     
-    def add_product(self, id, name, price):
+    def add_product(self, instance:ProductModel):
         for product in self.selected_products:
-            if product['id']== id:
+            if product['id']== instance.id:
                 product['quantity'] += 1
                 return
         
         self.selected_products.append({
-            'id': id,
-            'name': name,
-            'price':price,
+            'id': instance.id,
+            'name': instance.name,
+            'price':instance.price,
             'quantity':1,
         })
 
