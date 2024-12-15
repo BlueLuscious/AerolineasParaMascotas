563c36404f25e3664fc8df50adbde2ab316caf60
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2024-12-15 19:58:42 -0300

    Enhance styles 2
    - Se actualizo inclusiontag custom_button
    - Se actuliazaron los estilos de formulario de cotizacion.

--- airlines/web/templatetags/inclusiontag/custom_button.py
+++ airlines/web/templatetags/inclusiontag/custom_button.py
@@ -6,12 +6,13 @@ register = template.Library()
 
 
 @register.inclusion_tag("components/custom-button.html")
-def custom_button(url: str, target: str, label: str, tailwind_class: str = "") -> dict:
+def custom_button(label: str, url: str = "", target: str= "", type: str = "button", btn_tailwind: str = "") -> dict:
     context = {
+        "my_label": label,
         "my_url": url,
         "my_target": target,
-        "label": label,
-        "tailwind_class": tailwind_class,
+        "type": type,
+        "btn_tailwind": btn_tailwind,
     }
     logger.info(f"category card context: {context}")
     return context
