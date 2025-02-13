98de3a2f06710a8fe743d7cb03be45c83545a341
Barba Satan <gustavoghioldi@gmail.com>
2025-02-13 17:28:26 -0300

    acl bucket to public

--- airlines/airlines/settings.py
+++ airlines/airlines/settings.py
@@ -203,6 +203,8 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL')
 
+AWS_DEFAULT_ACL = 'public-read'
+
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
