e748c4809f7c41d32cbbfa17c84e92f802bd7227
Barba Satan <gustavoghioldi@gmail.com>
2025-02-13 18:31:38 -0300

    quita firma de la imagen

--- airlines/airlines/settings.py
+++ airlines/airlines/settings.py
@@ -204,7 +204,7 @@ AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL')
 
 AWS_DEFAULT_ACL = 'public-read'
-
+AWS_QUERYSTRING_AUTH = False
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
