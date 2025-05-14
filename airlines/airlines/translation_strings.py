0edeb7cb62afc3405264abd6964c19c16bc48788
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2025-05-14 18:09:20 -0300

    I18N --> Create --> Translation strings file.
    Translate --> Navbar & Sidebar.
    
    Create --> Language Selector template.

---
+++ airlines/airlines/translation_strings.py
@@ -0,0 +1,12 @@
+from django.utils.translation import gettext as _
+
+# Navigation
+_("Inicio")
+_("Quienes somos")
+_("Servicios")
+_("Destinos")
+_("Contacto")
+_("Español")
+_("Inglés")
+_("Menú")
+
