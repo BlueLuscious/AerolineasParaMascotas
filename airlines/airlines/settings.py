8e40f0e1918bf9e9c7a7c3e2517f3bcda895aa7a
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2025-09-11 02:11:16 -0300

    Change --> Base Language

--- airlines/airlines/settings.py
+++ airlines/airlines/settings.py
@@ -165,7 +165,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
